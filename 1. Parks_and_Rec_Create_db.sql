cb0c64346cfaeb13bd345dce681dd37e08042ced
Satyajeet Mahato <maximus@Satyajeets-MacBook-Air.local>
2025-01-20 11:42:37 +0530

    Created new sql file 1. Parks_and_Rec_Create_db.sql

---
+++ 1. Parks_and_Rec_Create_db.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS 'Parks_and_Recreation';
+CREATE DATABASE 'Parks_and_Recreation';
+USE 'Parks_and_Recreation';
+
+
+CREATE TABLE employee_demographics (
+    employee_id INT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10),
+    birth_date DATE,
+    PRIMARY KEY (employee_id)
+);
